91e5412aee963ef4c04a69e8d115cf0a4cd8cc1a
Amappola <anam.porrasp@gmail.com>
2023-09-14 06:59:50 -0500

    Adding FormsModule to the imports of app.module to be able to use ngModel in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeroDetailComponent } from './components/hero-detail/hero-detail.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

20f36767c4c55a7b819aa3148c47e9ef325deaa1
Amappola <anam.porrasp@gmail.com>
2023-09-18 08:29:20 -0500

    Configuring the route for the messages component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 const routes: Routes = [
   {path:'dashboard', component: DashboardComponent},
   {path: 'heroes', component: HeroesListComponent},
+  {path: 'messages', component: MessagesComponent},
   {path:'**', redirectTo:'dashboard'}
 ];
 

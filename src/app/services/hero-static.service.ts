8884f5431937e73e115729f0fb4fa8656d258fe2
Amappola <anam.porrasp@gmail.com>
2023-09-18 08:47:08 -0500

    Injecting the message service into the hero-static service (service-in-service)

--- src/app/services/hero-static.service.ts
+++ src/app/services/hero-static.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from '../data/mock';
 import { Hero } from '../interfaces/hero';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroStaticService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Hero[] {
+    this.messageService.addMessage('Hero Service: List of heroes fetched');
     return HEROES
   }
 
   getTopHeroes(minScore: number): Hero[] {
+    this.messageService.addMessage('Hero Service: List of top heroes fetched')
     return HEROES.filter((hero: Hero) => hero.score > minScore);
   }
 }

d04090a6fb8f171a82576cc484a6e69ffa9bc92c
Amappola <anam.porrasp@gmail.com>
2023-09-18 09:18:46 -0500

    Adding a message to the message service when a hero is selected in the dashboard component or the hero-list component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from 'src/app/interfaces/hero';
 import { HeroStaticService } from 'src/app/services/hero-static.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +10,16 @@ import { HeroStaticService } from 'src/app/services/hero-static.service';
 })
 export class DashboardComponent {
   topHeroes: Hero[] = [];
+  selectedHero!: Hero;
 
-  constructor(private heroStaticService: HeroStaticService) {}
+  constructor(private heroStaticService: HeroStaticService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.topHeroes = this.heroStaticService.getTopHeroes(1000);
+  }
+
+  onSelectHero(selectedHero: Hero): void {
+    this.selectedHero = selectedHero;
+    this.messageService.addMessage(`Dashboard component: Selected hero id = ${selectedHero.id}, name = ${selectedHero.name}`)
   }
 }
--- src/app/components/heroes-list/heroes-list.component.ts
+++ src/app/components/heroes-list/heroes-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from 'src/app/interfaces/hero';
 import { HeroStaticService } from 'src/app/services/hero-static.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-heroes-list',
@@ -11,7 +12,7 @@ export class HeroesListComponent {
   heroesList: Hero[] = [];
   selectedHero!: Hero;
 
-  constructor(private heroStaticService: HeroStaticService) {}
+  constructor(private heroStaticService: HeroStaticService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -19,5 +20,10 @@ export class HeroesListComponent {
 
   getHeroes(): void {
     this.heroesList = this.heroStaticService.getHeroes();
+  }
+
+  onSelectHero(selectedHero: Hero): void {
+    this.selectedHero = selectedHero;
+    this.messageService.addMessage(`Heroes List component: Selected hero id = ${selectedHero.id}, name = ${selectedHero.name}`)
   }
 }
